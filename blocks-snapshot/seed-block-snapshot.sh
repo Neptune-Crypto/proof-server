aca88b46eb7ccb52225fb9101414cf842fcca858
Thorkil Værge <thorkilk@gmail.com>
2025-07-28 11:44:57 +0200

    Update seed-block-snapshot.sh

--- blocks-snapshot/seed-block-snapshot.sh
+++ blocks-snapshot/seed-block-snapshot.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# A program to create a snapshot of blocks and share as a torrent.
-# Example use, with cron
-# 0 2 * * 0 /usr/local/bin/create_and_seed_weekly_torrent_main_net.sh >> /var/log/create_weekly_torrent.log 2>&1
-
 set -euo pipefail
 
-# Add name/nick name/company name here (no whitespace)
-USER_TAG=""
 SOURCE_DIR="$HOME/.local/share/neptune/main/blocks/"
 DEST_DIR="/srv/torrents"
 TRACKERS=(
@@ -19,9 +13,10 @@ TRACKERS=(
 
 KEEP_COUNT=1
 TIMESTAMP=$(date +%Y-%m-%d)
-FILENAME_PREFIX="neptune-mainnet-blocks-${USER_TAG}"
+FILENAME_PREFIX="neptune-mainnet-blocks-<tag>"
 TARBALL="${DEST_DIR}/${FILENAME_PREFIX}-${TIMESTAMP}.tar.gz"
 TORRENT="${TARBALL}.torrent"
+AUTH="<transmission-user>:<transmission-password>"
 
 mkdir -p "$DEST_DIR" # Make sure you have the priviliges to do this
 
@@ -33,19 +28,20 @@ tar cvf - "$SOURCE_DIR" -P | pv -s $(du -sb "$SOURCE_DIR" | awk '{print $1}') > 
 echo "Done creating tarball"
 
 
-# Step 2: Remove old torrents
-TORRENT_LIST=$(transmission-remote --list | grep -v "ID\|Sum:" | awk '{print $1}' | sed 's/\*//g' | grep -E '^[0-9]+$')
+# Step 2: Remove old torrents, allow for empty --list output with `|| true`.
+echo "Getting list. AUTH is '$AUTH'"
+TORRENT_LIST=$(transmission-remote -n "$AUTH" --list || true | grep -v "ID\|Sum:" | awk '{print $1}' | sed 's/\*//g' | grep -E '^[0-9]+$')
 
+echo "Removing old torrents"
 if [ -n "$TORRENT_LIST" ]; then
     # Remove existing torrents
-    echo "$TORRENT_LIST" | xargs -I{} -n 1 transmission-remote -t {} --remove || {
+    echo "$TORRENT_LIST" | xargs -I{} -n 1 transmission-remote -n "$AUTH" -t {} --remove || {
         echo "Failed to remove existing torrents"
         exit 1
     }
 else
     echo "No torrents to remove"
 fi
-
 
 # Step 3: Create torrent with multiple trackers
 TRACKER_ARGS=()
@@ -54,32 +50,32 @@ for TRACKER in "${TRACKERS[@]}"; do
 done
 
 echo "Creating torrent"
-transmission-create -o "$TORRENT" "${TRACKER_ARGS[@]}" "$TARBALL"
+transmission-create -n "$AUTH" -o "$TORRENT" "${TRACKER_ARGS[@]}" "$TARBALL"
 echo "Done creating torrent"
 
 
-# Step 4: Move files to seeding directory
+# Step 3: Move files to seeding directory
 echo "Moving files to seeding directory"
 SEED_DIR="/var/lib/transmission-daemon/downloads"
 cp "$TARBALL" "$SEED_DIR"
 echo "Done moving files"
 
 
-# Step 5: Reload transmission-daemon to pick up new torrent
+# Step 4: Reload transmission-daemon to pick up new torrent
 echo "Reloading transmission daemon to pick up new torrent"
-transmission-remote --add "$TORRENT"
+transmission-remote -n "$AUTH" --add "$TORRENT"
 echo "Done reloading"
 
 
-# Step 6: Serve torrent file from "proof server"
-PUBLIC_TORRENT="/var/www/neptune-core-proofs/latest-snapshot.torrent"
+# Step 5: Serve torrent file from "proof server"
+PUBLIC_TORRENT="/var/www/<http-server-name>/latest-snapshot.torrent"
 echo "Copy torrent file to file server directory"
 cp "$TORRENT" "$PUBLIC_TORRENT"
 chmod 664 "$PUBLIC_TORRENT"
 echo "Done copying torrent file to file server directory"
 
 
-# Step 7: Cleanup old tarballs and torrents (keep last $KEEP_COUNT)
+# Step 6: Cleanup old tarballs and torrents (keep last $KEEP_COUNT)
 echo "Cleaning up old tarballs in $DEST_DIR"
 cd "$DEST_DIR"
 (ls -tp | grep -E "^${FILENAME_PREFIX}-.*\.tar\.gz$" | tail -n +$((KEEP_COUNT + 1)) | xargs -r rm)
